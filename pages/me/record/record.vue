fee694dbe98966bd2be931789b8afde0c1748c1c
223 <l15723746653@outlook.com>
2024-11-15 13:23:14 +0800

    销售记录完成

--- pages/me/record/record.vue
+++ pages/me/record/record.vue
@@ -4,21 +4,23 @@
     <view class="item" v-for="item in recordList" :key="item.id">
       <view class="user">
         <text>分销用户:</text>
-        <image :src="item.avatar" mode="scaleToFill"
-          style="margin-left: 10rpx;width: 50rpx; height: 50rpx; border-radius: 50%;" />
+        <image :src="item.avatar" mode="scaleToFill" class="avatar" />
         <text>{{ item.user_name }}</text>
       </view>
-      <view class="main">
-        <image :src="item.path" mode="scaleToFill" style="width:160rpx; height:160rpx;border-radius: 15rpx;" />
+      <view class="main" v-for="good in item.goods" :key="good.id">
+        <image :src="good.path" mode="scaleToFill" style="width:160rpx; height:160rpx;border-radius: 15rpx;" />
         <view class="right">
-          <text class="name">{{ item.name }}</text>
-          <text class="size">{{ item.item_name }}</text>
-          <text class="number">x{{ item.cont }}</text>
+          <text class="name">{{ good.name }}</text>
+          <text class="size">{{ good.item_name }}</text>
+          <text class="number">x{{ good.cont }}</text>
           <view class="bottom">
             <view class="tags">
-              <text class="tag" v-for="(tag, index) in item.labels" :key="index">{{ tag }}</text>
+              <!-- <text class="tag" v-for="(tag, index) in good.labels" :key="index">{{ tag }}</text> -->
+              <TnTag type="success" :custom-style="{ marginRight: '10rpx' }" v-for="(tag, index) in good.labels"
+                :key="index">
+                {{ tag }}</TnTag>
             </view>
-            <view class="price">已赚 ￥{{ item.up_price }}</view>
+            <view class="price">已赚 ￥{{ good.up_price }}</view>
           </view>
         </view>
       </view>
@@ -29,6 +31,7 @@
 <script setup>
 import { ref } from 'vue'
 import { onShow } from '@dcloudio/uni-app'
+import TnTag from '@tuniao/tnui-vue3-uniapp/components/tag/src/tag.vue'
 import { get_userList } from '@/api/surplus/surplus'
 import Header from '@/components/header.vue'
 
@@ -38,6 +41,49 @@ const recordList = ref([])
 const getData = () => {
   get_userList().then(res => {
     recordList.value = res.data.data
+    recordList.value.push(
+      {
+        id: 1,
+        user_name: '张三',
+        avatar: 'https://picsum.photos/50/50',
+        goods: [
+          {
+            id: 1,
+            name: '商品名称',
+            item_name: '商品规格',
+            path: 'https://picsum.photos/160/160',
+            cont: 1,
+            labels: ['标签1', '标签2'],
+            up_price: 10
+          },
+          {
+            id: 2,
+            name: '商品名称',
+            item_name: '商品规格',
+            path: 'https://picsum.photos/160/160',
+            cont: 1,
+            labels: ['标签1', '标签2'],
+            up_price: 10
+          },
+        ]
+      },
+      {
+        id: 2,
+        user_name: '李四',
+        avatar: 'https://picsum.photos/50/50',
+        goods: [
+          {
+            id: 2,
+            name: '商品名称',
+            item_name: '商品规格',
+            path: 'https://picsum.photos/160/160',
+            cont: 1,
+            labels: ['标签1', '标签2'],
+            up_price: 10
+          }
+        ]
+      }
+    )
   })
 }
 
@@ -54,16 +100,23 @@ onShow(() => {
   width: 100%;
   min-height: 100vh;
   background: #F7F7F7;
-  padding: 120rpx 0 50rpx;
+  padding: 120rpx 20rpx 50rpx;
 
   .item {
-    width: 90%;
+    width: 100%;
     margin-top: 50rpx;
 
     .user {
       display: flex;
       align-items: center;
       margin-bottom: 5rpx;
+
+      .avatar {
+        margin: 0 10rpx;
+        width: 50rpx;
+        height: 50rpx;
+        border-radius: 50%;
+      }
     }
 
     .main {
@@ -123,31 +176,13 @@ onShow(() => {
             align-items: center;
             justify-content: start;
             flex-wrap: wrap;
-            max-width: 200rpx;
-
-            .tag {
-              max-width: 200rpx;
-              padding: 3rpx 10rpx;
-              background: #FAEBD9;
-              border-radius: 6rpx 6rpx 6rpx 6rpx;
-              font-family: Inter, Inter;
-              font-weight: 500;
-              font-size: 17rpx;
-              color: #A79A77;
-              line-height: 26rpx;
-              text-align: center;
-              font-style: normal;
-              text-transform: none;
-              margin-right: 10rpx;
-              margin-bottom: 10rpx;
-            }
           }
 
           .price {
             font-family: Inter, Inter;
             font-weight: 700;
             font-size: 35rpx;
-            color: #997D5D;
+            color: #EE2532;
             line-height: 43rpx;
             text-align: right;
             font-style: normal;
